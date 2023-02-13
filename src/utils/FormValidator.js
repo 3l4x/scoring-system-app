b11eddec4017496700cc205990e66119cefe4f8b
3l4x <c0ltwinters@protonmail.com>
2023-02-13 17:12:03 +0100

    Shorten validation error message for NaN

--- src/utils/FormValidator.js
+++ src/utils/FormValidator.js
@@ -8,8 +8,15 @@ export const FormValidator = async (schema, formValues) => {
         //failure
 /*         console.log('fail');
         console.log(err.inner); */
-        return err.inner.reduce((acc, { path, message }) => ({
-            ...acc, [path]: (message.charAt(0).toUpperCase() + message.slice(1)).replace(path,'This')
-        }), {});
+        return err.inner.reduce((acc, { path, message }) => {
+            if(message.includes('NaN'))
+            {
+                return {
+                    ...acc,
+                    [path] : 'Please enter a valid number'
+                }
+            }
+            return {...acc, [path]: (message.charAt(0).toUpperCase() + message.slice(1)).replace(path,'This')}
+        }, {});
     }
 }
